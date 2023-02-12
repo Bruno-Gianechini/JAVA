7b719b3198821e743bc62e99d0225513a29749d6
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-02-12 08:36:08 -0300

    ____________________________________________________________________________________________
    [2023.02.12]
    - SmartTv.java: Contém atributos e métodos referente a classe SmartTv.
    - Objetivo: Aplicar os conceitos de método e verificar sua atuação na prática.
    ____________________________________________________________________________________________
    
    [2023.02.12]
    - Usuario.java: Interage com o objeto SmartTv.
    - Objetivo: Interagir através de métodos para alterar o estado e atributos da classe.
    ____________________________________________________________________________________________

--- aprendendo-a-sintaxe-java/exercicio-metodo-smart-tv/src/SmartTv.java
+++ aprendendo-a-sintaxe-java/exercicio-metodo-smart-tv/src/SmartTv.java
@@ -1,56 +1,70 @@
-public class SmartTv {
-    boolean ligada = false;
-    int canal = 1;
-    int volume = 25;
+//  INÍCIO CLASSE SMARTTV.
+    public class SmartTv 
+{
+    //  INÍCIO ATRIBUTOS.
+        boolean ligada = false;
+        int canal = 1;
+        int volume = 25;
+    //  FIM ATRIBUTOS.
 
-    // METODOS
+    //  INÍCIO MÉTODOS.
 
-        // LIGAR TV.
-        public void ligar()
+        //  INÍCIO LIGAR TV.
+            public void ligar()
             {
                 System.out.println("!! - Ligando a TV");
                 ligada = true;
             }
+        //  FIM LIGAR TV.
             
-        // DESLIGAR TV.
-        public void desligar()
+        //  INÍCIO DESLIGAR TV.
+            public void desligar()
             {
                 ligada = false;
                 System.out.println("!! - Desligando a TV");
             }
+        //  FIM DESLIGAR TV.
         
-        // AUMENTAR VOLUME.
-        public void aumentarVolume()
+        //  INÍCIO AUMENTAR VOLUME.
+            public void aumentarVolume()
             {   
                 volume++;
                 System.out.println("!! - Aumentando o volume: " + volume);
             }
-    
-        // DIMINUIR VOLUME.
-        public void diminuirVolume()
+        //  FIM AUMENTAR VOLUME.
+
+        //  INÍCIO DIMINUIR VOLUME.
+            public void diminuirVolume()
             {   
                 volume--;
                 System.out.println("!! - Diminuindo o volume: " + volume);
             }
-        
-        // AUMENTAR CANAL.
-        public void aumentarCanal()
+        //  FIM DIMINUIR VOLUME.
+
+        //  INÍCIO AUMENTAR CANAL.
+            public void aumentarCanal()
             {   
                 canal++;
                 System.out.println("!! - Aumentando o canal: " + canal);
             }
+        //  FIM AUMENTAR CANAL.
 
-        // ESCOLHER CANAL.
-        public void escolherCanal(int novoCanal)
+        //  ESCOLHER CANAL.
+            public void escolherCanal(int novoCanal)
             {
                 canal = novoCanal;
                 System.out.println("!! - Canal mudado para: " + canal);
             }
-    
-        // DIMINUIR CANAL.
-        public void diminuirCanal()
+        //  FIM ESCOLHER CANAL.
+
+        //  INÍCIO DIMINUIR CANAL.
+            public void diminuirCanal()
             {   
                 System.out.println("!! - Diminuindo o canal: " + canal);
                 canal--;
             }
+        //  FIM DIMINUIR CANAL.
+
+    //  FIM MÉTODOS.
 }
+//  FIM CLASSE SMARTTV.
