49b22f895a72eb7e03d3b80a36ec0edf1cd30501
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-02-12 09:15:07 -0300

    Adicionada arquivo json para argumentos.

--- aprendendo-a-sintaxe-java/About-Me/AboutMe/src/AboutMe.java
+++ aprendendo-a-sintaxe-java/About-Me/AboutMe/src/AboutMe.java
@@ -8,15 +8,16 @@ public class AboutMe
                 //  INÍCIO ATRIBUTOS
                     String nome = args [0];
                     String sobrenome = args [1];
-                    int idade = Integer.valueOf(args[2]);
-                    double altura = Double.valueOf(args[3]);
+                    int idade = Integer.valueOf(args [2]);
+                    double altura = Double.valueOf(args [3]);
+                    String mensagem = args [4];
                 //  FIM ATRIBUTOS
 
                 //  INÍCIO SAÍDA
                     System.out.println("Olá, me chamo " + nome + " " + sobrenome);
                     System.out.println("Tenho " + idade + " anos");
                     System.out.println("E minha altura é de " + altura + " cm.");
-                    System.out.println("Sejam muito bem vindos(as) ao meu humilde perfil!!");
+                    System.out.println(mensagem);
                 //  FIM SAÍDA
             }
     }
