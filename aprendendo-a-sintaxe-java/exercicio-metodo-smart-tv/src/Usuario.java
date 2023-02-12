e84ff8f8ef7ff42c64d927b90e0a146cd1606cd3
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-02-12 08:40:42 -0300

    2023.02.12

--- aprendendo-a-sintaxe-java/exercicio-metodo-smart-tv/src/Usuario.java
+++ aprendendo-a-sintaxe-java/exercicio-metodo-smart-tv/src/Usuario.java
@@ -1,25 +1,27 @@
-public class Usuario {
-    public static void main(String[] args) throws Exception {
+//  INÍCIO CLASSE USUÁRIO.
+    public class Usuario 
+{
+    public static void main(String[] args) throws Exception 
+    {
         SmartTv smartTv = new SmartTv();
 
-        // ESTADO INICIAL DA TV.
+        //  INÍCIO ESTADO INICIAL DA TV.
             System.out.println("TV Ligada: " + smartTv.ligada);
             System.out.println("Canal atual: " + smartTv.canal);
             System.out.println("Volume atual: " + smartTv.volume);
+        //  FIM ESTADO INICIAL DA TV.
 
-        // INTERAÇÕES DO USUÁRIO.
+        //  INÍCIO INTERAÇÕES DO USUÁRIO.
             smartTv.ligar();
             System.out.println("TV Ligada: " + smartTv.ligada);
-
             smartTv.diminuirVolume();
             smartTv.diminuirVolume();
             smartTv.diminuirVolume();
             smartTv.aumentarVolume();
-            System.out.println("Volume atual: " + smartTv.volume);
-
             smartTv.aumentarCanal();
             smartTv.aumentarCanal();
-
             smartTv.escolherCanal(13);
+        //  FIM INTERAÇÕES DO USUÁRIO.
     }
 }
+//  FIM CLASSE USUÁRIO.
