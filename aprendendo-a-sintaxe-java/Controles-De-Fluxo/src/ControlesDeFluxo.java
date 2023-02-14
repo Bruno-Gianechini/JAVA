09f6db7f1808ccb9e0c70cebe0355375ee7fc22b
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-02-13 21:12:40 -0300

    2023.02.13 - 21:12 - Criação "ControlesDeFluxo".

---
+++ aprendendo-a-sintaxe-java/Controles-De-Fluxo/src/ControlesDeFluxo.java
@@ -0,0 +1,23 @@
+//  INÍCIO CLASSE.
+    public class ControlesDeFluxo 
+{
+    public static void main(String[] args) throws Exception 
+        {
+            // INÍCIO TEORIA CONTROLES DE FLUXO.
+            /*
+               São estruturas capazes de alterar o fluxo de execução de um código.
+               seus tipos são:
+                - Decisão: if, if-else, if-else-if, switch e operador ternário.
+                - Repetição: for, while e do while.
+                - Interrupção: break, continue e return.
+                
+                Boas práticas:
+                - Switch é para valores exatos e if para expressões booleanas.
+                - Evitar usar o default do witch para "cases genéricos".
+                - Evitar o efeito "Flecha" dos if's.
+            */ 
+            //  FIM TEORIA CONTROLES DE FLUXO.
+
+        }
+}
+//  FIM CLASSE.
