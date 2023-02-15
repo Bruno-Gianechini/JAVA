461fe83b1af25569d8f2fd70cb414c498344d9ed
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-02-15 15:34:27 -0300

    2023.02.15 - 15:34 - Alterações realizadas.

--- aprendendo-a-sintaxe-java/EstruturaDeRepeticao-e-Arrays/src/Ex6_Fatorial.java
+++ aprendendo-a-sintaxe-java/EstruturaDeRepeticao-e-Arrays/src/Ex6_Fatorial.java
@@ -8,35 +8,44 @@ public class Ex6_Fatorial
     {
         //  INÍCIO EXERCÍCIO.
 
-            /*  EXERCÍCIO 6 - FATORIAL.
-                Faça um programa que calcule o valor resultante de um número fatorial.
-                Exemplo: 4! = 4 * 3 * 3 * 1 = 24.
+            /*  
+                EXERCÍCIO 6 - FATORIAL.
+                 Faça um programa que calcule o valor resultante de um número fatorial.
+                 Exemplo: 4! = 4 * 3 * 3 * 1 = 24.
             */
 
             //  INÍCIO VARIÁVEIS.
-                Scanner scanner = new Scanner(System.in);
+                Scanner scanner = new Scanner(System.in);           //  Aguarda a inserção de um dado por um usuário.
                 int numeroDesejado;
                 int contador;
-                int multiplicador;
                 int resultado;
             //  FIM VARIÁVEIS.
 
             //  INÍCIO SOLUÇÃO.
 
-                System.out.println("\nDigite o número e descubra o resultado de seu fatorial: ");
-                numeroDesejado = scanner.nextInt();
-                contador = numeroDesejado-1;
-                resultado = numeroDesejado;
-                do
-                {
-                resultado = (resultado*contador);
-                contador--;
-                }
-                while(contador != 1);
-                
-                System.out.println("\nRESULTADO:");
-                System.out.println(numeroDesejado + "! é igual a: " + resultado);
-                System.out.println("PROGRAMA FINALIZADO!!\n");
+                //  INÍCIO ENTRADA DE DADOS.
+                    System.out.println("\nDigite o número e descubra o resultado de seu fatorial: ");
+
+                    numeroDesejado = scanner.nextInt();             //  Recebe o valor inserido pelo usuário.
+                    resultado = numeroDesejado;                     //  Recebe o valor inserido pelo usuário.
+                    contador = numeroDesejado-1;                    //  Salva o primeiro número a multiplicar e serve como contagem para fatoração.
+                //  FIM ENTRADA DE DADOS.
+
+                    //  INÍCIO DA REPETIÇÃO.
+                        do
+                        {
+                           resultado = (resultado*contador);        //  Recebe o resultado da multiplicação entre o resultado e o contador.
+                           contador --;                             //  Diminui o contador em 1 para que a próxima multiplicação com o resultado mantenha a continuidade do cálculo fatorial.
+                        }
+                        while(contador != 1);                       //  Realiza a repetição do bloco até que o resultado do contador se torne 1.
+                    //  FIM DA REPETIÇÃO.
+
+                //  INÍCIO SAÍDA DE DADOS.
+                    System.out.println("\nRESULTADO:");
+                    System.out.println(numeroDesejado + "! é igual a: " + resultado);
+                    System.out.println("PROGRAMA FINALIZADO!!\n");
+                //  FIM SAÍDA DE DADOS.
+
             //  FIM SOLUÇÃO.
 
         //  FIM EXERCÍCIO.
