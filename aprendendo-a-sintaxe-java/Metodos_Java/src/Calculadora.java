fead04fb2aad2bd2a9475a5c6d22a9161845addf
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-03-02 10:23:12 -0300

    2023.03.02 - Calculadora criada com sucesso.

--- aprendendo-a-sintaxe-java/Metodos_Java/src/Calculadora.java
+++ aprendendo-a-sintaxe-java/Metodos_Java/src/Calculadora.java
@@ -1,10 +1,51 @@
-import java.lang.reflect.Method;
+import java.util.Scanner;
 
-//  INÍCIO CLASSE
+//  INÍCIO CLASSE.
 public class Calculadora
-{
-    public static void main(String[] args) throws Exception 
-    {
-            
-    }
+{   
+    
+    /*
+        EXERCÍCIO CALCULADORA. 
+        Crie um programa capaz de receber dois valores inteiros.
+        e então realizar uma das quatro operações matemáticas conforme desejado.
+    */
+
+    //  INÍCIO MÉTODOS.
+        
+        //  SOMAR.
+            public static void somar(float x, float y)
+            {
+                float resultado = (x+y);
+                System.out.println("RESULTADO SOMA:");
+                System.out.println(x + "+" + y + " é igual a: " + resultado);
+            }
+
+        //  SUBTRAIR.
+            public static void subtrair(float x, float y)
+            {
+                float resultado = (x-y);
+                System.out.println("\nRESULTADO SUBTRAÇÃO:");
+                System.out.println(x + "-" + y + " é igual a: " + resultado);
+            }
+
+        //  MULTIPLICAR.
+            public static void multiplicar(Double x, Double y)
+            {
+                Double resultado = (x*y);
+                System.out.println("\nRESULTADO MULTIPLICAÇÃO:");
+                System.out.println(x + "x" + y + " é igual a: " + resultado);
+            }
+
+        //  DIVIDIR.
+            public static void dividir(Double x, Double y)
+            {
+                Double resultado = (x/y);
+                System.out.println("\nRESULTADO DIVISÃO:");
+                System.out.println(x + "/" + y + " é igual a: " + resultado);
+                System.out.println("\nPROGRAMA CÁLCULO FINALIZADO!! \n");
+            }
+
+    //  FIM MÉTODOS.
+
 }
+//  FIM CLASSE.
