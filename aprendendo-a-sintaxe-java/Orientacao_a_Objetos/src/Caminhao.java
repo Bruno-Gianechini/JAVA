cf7fe515f7e60dcac7f25a823f00f9db9d19b716
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-03-03 21:04:43 -0300

    2023.03.03 - 21.04 - Classes filhas criadas

---
+++ aprendendo-a-sintaxe-java/Orientacao_a_Objetos/src/Caminhao.java
@@ -0,0 +1,8 @@
+public class Caminhao extends Veiculo {
+
+    Caminhao(String marca, String modelo, String cor, int rodas, int tanque) {
+        super(marca, modelo, cor, rodas, tanque);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ aprendendo-a-sintaxe-java/Orientacao_a_Objetos/src/Moto.java
@@ -0,0 +1,8 @@
+public class Moto extends Veiculo {
+
+    Moto(String marca, String modelo, String cor, int rodas, int tanque) {
+        super(marca, modelo, cor, rodas, tanque);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
