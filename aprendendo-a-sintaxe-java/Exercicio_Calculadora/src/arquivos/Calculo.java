12786b8e5daf6f31728723fab4ed436bb00c1c23
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-03-06 18:15:30 -0300

    2023.03.06 - Finalizado.

---
+++ aprendendo-a-sintaxe-java/Exercicio_Calculadora/src/arquivos/Calculo.java
@@ -0,0 +1,19 @@
+package arquivos;
+
+public class Calculo {
+    public static double somar(double x, double y){
+        return x+y;
+    }
+
+    public static double subtrair(double x, double y){
+        return x-y;
+    }
+
+    public static double dividir(double x, double y){
+        return x/y;
+    }
+
+    public static double multiplicar(double x, double y){
+        return x*y;
+    }
+}
