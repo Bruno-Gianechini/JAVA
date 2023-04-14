856669c92d137571d34c5b593ef68da8658a5576
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-04-14 17:29:05 -0300

    Projeto Spring

---
+++ Desafio_Padroes_Projeto_Spring/src/main/java/one/digitalinnovation/Desafio_Padroes_Projeto_Spring/DesafioPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.Desafio_Padroes_Projeto_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPadroesProjetoSpringApplication.class, args);
+	}
+
+}
