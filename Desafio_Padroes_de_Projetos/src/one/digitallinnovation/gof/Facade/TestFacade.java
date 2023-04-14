cbaa2b7d3a672cfcae1b4a3410f14ac58e0af508
Bruno-Gianechini <bruno.gianechini.jr@gmail.com>
2023-04-14 16:16:27 -0300

    Criação do padrão facade

---
+++ Desafio_Padroes_de_Projetos/src/one/digitallinnovation/gof/Facade/TestFacade.java
@@ -0,0 +1,10 @@
+package one.digitallinnovation.gof.Facade;
+
+public class TestFacade {
+
+    public static void main(String[] args) {
+    
+Facade facade = new Facade();
+facade.migrarCliente("Venilton", "14801788");
+}
+}
